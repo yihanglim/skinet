38e65baed8e59169cfc2cc2fb546836adc43e005
Xavier <yihanglim@yahoo.com>
2020-07-28 18:29:48 +0800

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Core.Entities
+{
+	public class Product
+	{
+		
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+	}
+}
+
+
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
